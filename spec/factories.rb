51589a739552abe4397dc683b2e029887cf62e68
Carlos <j.morin@ukko.mx>
2023-01-09 15:24:16 -0600

    Factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,4 +1,12 @@
 FactoryBot.define do
+  factory :product do
+    name { "MyString" }
+    price { 1.5 }
+    status { 1 }
+    quantity { 1 }
+    user { nil }
+  end
+
   factory(:user) do
     email { Faker::Internet.email }
     password { Faker::Internet.password }
