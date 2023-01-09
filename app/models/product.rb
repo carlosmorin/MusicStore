9a61e3ef8f72d60bd9e08afae2a9668bb3ba706f
Carlos <j.morin@ukko.mx>
2023-01-09 15:29:14 -0600

    Models relations and validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, inverse_of: :products
+
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :quantity, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,12 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :last_name, presence: true
 
+  has_many :products, inverse_of: :user, dependent: :destroy
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def full_name
+    "#{name} #{last_name}"
+  end
 end
