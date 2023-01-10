280d5e8ad947b973b25c2001409effff4c074271
Carlos <j.morin@ukko.mx>
2023-01-10 00:06:11 -0600

    Action text set up

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -66,6 +66,6 @@ class ProductsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def product_params
-      params.require(:product).permit(:name, :price, :status, :quantity, :user_id, :picture)
+      params.require(:product).permit(:name, :price, :status, :quantity, :user_id, :picture, :description)
     end
 end
