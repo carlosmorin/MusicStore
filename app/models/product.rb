3f4c6209777ef1e6177a0fc467671ee1be01f064
Carlos <j.morin@ukko.mx>
2023-01-10 00:06:51 -0600

    IMplement action text to products

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,12 +2,14 @@ class Product < ApplicationRecord
   belongs_to :user, inverse_of: :products
   
   has_one_attached :picture
+  has_rich_text :description
 
   validates :name, presence: true
   validates :price, presence: true
   validates :quantity, presence: true
   validates :picture, presence: true
   validates :status, presence: true
+  validates :description, presence: true
 
   enum status: %i[inactive active]
 end
