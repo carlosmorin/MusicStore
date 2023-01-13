f8b2f64dd792273d305272bb788eec678c4943b9
Carlos <j.morin@ukko.mx>
2023-01-12 18:26:01 -0600

    Init cart in products module

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,26 +1,23 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: %i[ show edit update destroy ]
-  # GET /products or /products.json
+  include CartConcern
+
   def index
     @products = Product.all
   end
 
-  # GET /products/1 or /products/1.json
   def show
   end
 
-  # GET /products/new
   def new
     @product = Product.new
     authorize! :manage, @product
   end
 
-  # GET /products/1/edit
   def edit
     authorize! :manage, @product
   end
 
-  # POST /products or /products.json
   def create
     @product = Product.new(product_params)
     @product.user = current_user
@@ -36,7 +33,6 @@ class ProductsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /products/1 or /products/1.json
   def update
     respond_to do |format|
       if @product.update(product_params)
@@ -49,7 +45,6 @@ class ProductsController < ApplicationController
     end
   end
 
-  # DELETE /products/1 or /products/1.json
   def destroy
     @product.destroy
 
@@ -60,12 +55,10 @@ class ProductsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_product
       @product = Product.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def product_params
       params.require(:product).permit(:name, :price, :status, :quantity, :user_id, :picture, :description)
     end
