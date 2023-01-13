79770d8e8256eaecde4b65d1337e9de4d4413761
Carlos <j.morin@ukko.mx>
2023-01-12 18:28:58 -0600

    Correction in relation inverse of

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :user, inverse_of: :products
-  has_many :lists, inverse_of: :products, dependent: :destroy
+  has_many :lists, inverse_of: :product, dependent: :destroy
   
   has_one_attached :picture
   has_rich_text :description
